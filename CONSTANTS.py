61dc71a9ac526b51c6c31b1d9ffc0660ae4e51df
Arseni <1919ars@gmail.com>
2021-01-24 20:47:19 +0200

    notebook

---
+++ CONSTANTS.py
@@ -0,0 +1,6 @@
+import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
+import pandas as pd
+import plotly.graph_objects as go
---
+++ example_plotly.py
@@ -0,0 +1,14 @@
+import plotly.graph_objects as go
+
+import pandas as pd
+
+# Read data from a csv
+z_data = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/api_docs/mt_bruno_elevation.csv')
+
+fig = go.Figure(data=[go.Surface(z=z_data.values)])
+
+fig.update_layout(title='Mt Bruno Elevation', autosize=False,
+                  width=500, height=500,
+                  margin=dict(l=85, r=90, b=75, t=100))
+
+fig.show()
