700580fb0a41a493d3f3265ca8d34a4daada93ab
Arseni <1919ars@gmail.com>
2021-01-29 10:43:00 +0200

    first

---
+++ IMPORTS.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D # noqa: F401 unused import
+import pandas as pd
+import plotly.graph_objects as go
+import cufflinks as cf
+import chart_studio.plotly as py
+import seaborn as sns
+import plotly.express as px
+# from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+
+
+
+toy_data = {'a': 1, 'b': 2}
