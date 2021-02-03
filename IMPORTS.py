9c06309d7c6207415d5c4dd725af16b7169d448d
Arseni1919 <1919ars@gmail.com>
2021-02-03 16:56:26 +0200

    changes

--- IMPORTS.py
+++ IMPORTS.py
@@ -10,6 +10,24 @@ import seaborn as sns
 import plotly.express as px
 # from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
 
+import sys
+# sys.path.append("/home/arseni1919/PycharmProjects/NT_sandbox")
+
+
+import os
+import torch
+from torch import nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader, Dataset
+from torch.utils.data import random_split
+from torchvision import transforms
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import Callback
+from collections import namedtuple, deque
+import gym
+from pytorch_lightning.loggers.neptune import NeptuneLogger
 
 
 toy_data = {'a': 1, 'b': 2}
+
+LR = 1e-3  # learning rate
