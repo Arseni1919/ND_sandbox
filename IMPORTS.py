36396ae6cad56635e9b9b03fd06526fe5e854ba2
Arseni1919 <1919ars@gmail.com>
2021-02-03 10:00:21 +0200

    readme

--- IMPORTS.py
+++ IMPORTS.py
@@ -4,7 +4,7 @@ from mpl_toolkits.mplot3d import Axes3D # noqa: F401 unused import
 import pandas as pd
 import plotly.graph_objects as go
 import cufflinks as cf
-import chart_studio.plotly as py
+# import chart_studio.plotly as py
 import seaborn as sns
 import plotly.express as px
 # from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
--- README.md
+++ README.md
@@ -4,7 +4,7 @@ In this repo we will try to fit different models to the stock market data.
 
 ## Models to try
 
-- Simple NN
+- [ ] Simple NN
 - LSTM
 - Deep RL (DRL)
 - Multi-Agent RL (MARL)
